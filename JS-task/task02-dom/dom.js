7e9a7fe84dfb28afe7e5ad82d2580fcd52701330
SzarkaFruzsinaSzonja <fudzsi.fru@gmail.com>
2021-04-30 13:10:19 +0200

    commit push

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,21 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    const navElements = document.querySelectorAll('nav#link-list a');
+    navElements.forEach( element => {
+        if ( element.innerHTML.includes('outer-link') ) {
+            element.target ='_blank';
+            element.innerHTML = `<strong>${element.innerHTML}</strong>`;
+        }
+    })
 
+
+    document.querySelectorAll("nav").forEach((c) => {
+        c.style.display = "flex";
+        c.style.flexDirection = "column";
+        c.style.margin = "0px auto";
+        c.style.width = "30%";
+        c.style.border = "1px solid blue";
+        c.style.padding = "16px";
+      });
+}
 
 export { changeOuterLinks };
